e75d60d3f9ebc01829699bd8be4c13b90da3360e
Seyhmusonder <seyhmusabe@gmail.com>
2025-12-21 20:06:13 +0300

    admin yetki kontrolü ve silme iyileştirildi

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,12 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+// Çevresel değişkenlerin tanımlı olduğundan emin ol
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-console.log("URL Kontrol:", supabaseUrl) // Terminalde ne yazdığına bakacağız
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Dostum, .env.local dosyasını hala okuyamıyorum. Dosya yerini veya ismini kontrol et!")
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createBrowserClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
